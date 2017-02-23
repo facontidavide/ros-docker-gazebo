f9a9ecf776554c5ee96eaf1a17725d218cb7a904
Davide Faconti <davide.faconti@gmail.com>
2017-02-23 18:17:41 +0100

    Update launch_docker.sh

--- launch_docker.sh
+++ launch_docker.sh
@@ -2,16 +2,16 @@
 xhost +local:root
 
 nvidia-docker run -it \
---env="DISPLAY" 
---env="QT_X11_NO_MITSHM=1" 
---user=$USER \
+nvidia-docker run -it \
+--env="DISPLAY"  \
+--env="QT_X11_NO_MITSHM=1"  \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 --workdir="/home/$USER" \
 --volume="/home/$USER:/home/$USER" \
 --volume="/etc/group:/etc/group:ro" \
 --volume="/etc/passwd:/etc/passwd:ro" \
 --volume="/etc/shadow:/etc/shadow:ro" \
 --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
---volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 --volume="$HOME/host_docker:/home/user/host_docker" \
 -e LOCAL_USER_ID=`id -u $USER` \
 -e LOCAL_GROUP_ID=`id -g $USER` \
